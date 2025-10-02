6c759a69f4c30b658827ab8e82f64deb3c7220c8
kato-mizuki <mk1141629@gmail.com>
2025-10-02 15:12:03 +0900

    footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,21 +5,26 @@
         <section class="section12">
             <footer>
                 <div class="center10-container1">
+                    <!-- ロゴ -->
                     <img src="{{asset('img/1289_color.png') }}" class="logo2" >
+
+                    <!-- フッター情報リスト -->
                     <ul class="footer-information">
                         <li class="information-item">営業時間<br>xx:xx ~ xx:xx</li>
                         <li class="information-item">所在地<br>xxxxxxxxxxxxx</li>
                         <li class="information-item">TEL <i class="fa-solid fa-phone"></i><br>xxx-xxx-xxxx</li>
                         <li class="information-item">Mail✉<br>xxx-xxx-xxxx</li>
-                        <div class="top-btn">
-                            <li class="information-item"><a href="{{route('top')}}" id="page-top">TOP</a></li>
-                        </div>
+                        <!-- TOPボタンもliにする -->
+                        <li class="information-item top-btn">
+                            <a href="{{route('top')}}" id="page-top">TOP</a>
+                        </li>
                     </ul>
+                    <!-- SNSアイコン -->
                     <div class="sns">
                         <ul class="sns-ikon">
                             <li class="sns-links">
                                 <a href="https://www.instagram.com/ki_i_chi_official/" target="_blank">
-                                    <i class="fa-brands fa-instagram"></i>
+                                    <i class="fab fa-instagram"></i>
                                 </a>
                                 <a href="https://www.tiktok.com/@kiichi.corp_official" target="_blank">
                                     <i class="fa-brands fa-tiktok"></i>
